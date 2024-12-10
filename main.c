278f8def725a0ff3d8d2a7854bb58f1690942e91
ClebsAlexandre <clebsalexandre@gmail.com>
2024-12-10 19:54:45 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,25 @@
+// Projeto Final
+// Professor: Mayrton
+// Grupo: Clebson  Alexandre, Sergio Roberto
+// Tema: Ecommerce voltado para componentes de informática
+
+#include <stdio.h>
+#include <string.h>
+
+typedef struct{
+ 
+    char nome[50];
+    char marca[30];
+    int codigo;
+    int garantia;
+    char notaFiscal[20];
+    float preco;
+    char descricao[100];
+    char tipo[20];   
+} Produto; 
+
+int main() {
+ 
+
+    return 0;
+}
