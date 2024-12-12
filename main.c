60dc005c3c947534c86c20796c1b285146386cf4
ClebsAlexandre <clebsalexandre@gmail.com>
2024-12-11 23:48:34 -0300

    código atualizado e funcional

--- main.c
+++ main.c
@@ -1,90 +1,244 @@
-// Projeto Final
+// Projeto Final 
 // Professor: Mayrton
 // Grupo: Clebson  Alexandre, Sergio Roberto
-// Tema: Ecommerce voltado para componentes de informática
+// Tema: Sistema de Estoque e Venda para Componentes de Informática
 
 #include <stdio.h>
 #include <string.h>
 
-// registro para armazenar dados dos produtos //
-typedef struct produto{
-    int cod;
-    char nome[20];
-    char descr[100];
-    char unidade[3];
-    char validade[2];
-    char preco_venda[4];   
-} prod;
+// Registro para armazenar dados dos componentes
+typedef struct componente {
+    char nome[50];
+    char marca[30];
+    int codigo;
+    int garantia;
+    float preco;
+    char descricao[100];
+    char tipo[20];
+    char notaFiscal[20]; 
+    int quantidade;
+} comp;
 
+int cont_comp = -1;
 
 int menu();
+void cad_comp(comp *);
+void listar_comp(comp *);
+void att_estoque(comp *);
+void remover_comp(comp *);
+void buscar_comp(comp *);
+void vender(comp *);
 
-int main(int argc, char **argv){
+int main() {
     int opc;
+    comp c[5]; // Vetor para os componentes cadastrados
 
-    //vetor para os produtos cadastrados
-    prod p[5];
-
-    do{
+    do {
         opc = menu();
         switch (opc) {
-        case 1:
-            cad_prod(p);
-            break;
-        case 2:
-            listar_prod(p);
-            break;
-        case 3:
-            att_estoque(p);
-            break;
-        case 4:
-            vender(p);
-            break;
-        case 5:
-            printf("Saindo\n");
-            break;
+            case 1:
+                cad_comp(&c[cont_comp + 1]);
+                break;
+            case 2:
+                listar_comp(c);
+                break;
+            case 3:
+                att_estoque(c);
+                break;
+            case 4:
+                remover_comp(c);
+                break;
+            case 5:
+                buscar_comp(c);
+                break;
+            case 6:
+                vender(c);
+                break;
+            case 7:
+                printf("Saindo...\n");
+                break;
             default:
-            printf("Escolha outra opcao\n");
-            break;
+                printf("Escolha uma opção válida!\n");
+                break;
+        }
+    } while (opc != 7);
 
-        
-        }             
+    return 0;
+}
+
+int menu() {
+    int opc;
+    printf("--------------- MENU DE GERENCIAMENTO E VENDA DE COMPONENTES ---------------\n");
+    printf("1 - Cadastrar componente\n");
+    printf("2 - Listar Componentes\n");
+    printf("3 - Atualizar Estoque\n");
+    printf("4 - Remover Componente\n");
+    printf("5 - Buscar Componente\n");
+    printf("6 - Ir para Venda\n");
+    printf("7 - SAIR\n");
+    printf("Escolha uma opção: ");
+    scanf("%d", &opc);
+    return opc;
+}
+
+void cad_comp(comp *c) {
+    if (cont_comp >= 5) { 
+        printf("Limite de componentes atingido.\n");
+        return;
+    }
+    cont_comp++;
+
+    c->codigo = cont_comp + 1;
+
+    printf("Nome do Componente: ");
+    scanf(" %[^\n]", c->nome); 
+
+    printf("Marca: ");
+    scanf(" %[^\n]", c->marca);
+
+    printf("Garantia (em meses): ");
+    scanf("%d", &c->garantia);
+
+    printf("Preço: ");
+    scanf("%f", &c->preco);
+
+    printf("Descrição: ");
+    scanf(" %[^\n]", c->descricao);
+
+    printf("Tipo: ");
+    scanf(" %[^\n]", c->tipo);
+
+    printf("Quantidade em estoque: ");
+    scanf("%d", &c->quantidade);
+}
+
+void listar_comp(comp *c) {
+    if (cont_comp < 0) {
+        printf("Nenhum componente cadastrado.\n");
+        return;
     }
 
-    while(opc != 5);
-        return 0;
+    for (int i = 0; i <= cont_comp; i++) {
+        printf("\n--- Componente %d ---\n", i + 1);
+        printf("Código: %d\n", (c + i)->codigo);
+        printf("Nome: %s\n", (c + i)->nome);
+        printf("Marca: %s\n", (c + i)->marca);
+        printf("Garantia: %d meses\n", (c + i)->garantia);
+        printf("Preço: R$ %.2f\n", (c + i)->preco);
+        printf("Descrição: %s\n", (c + i)->descricao);
+        printf("Tipo: %s\n", (c + i)->tipo);
+        printf("Quantidade disponível: %d\n", (c + i)->quantidade);
+    }
 }
 
-int menu(){
-    int opc;
-    printf("--------------- MENU ---------------\n");
-    printf("1 - Cadastrar produto\n");
-    printf("2 - Listar Produtos\n");
-    printf("3 - Atualizar Estoque (INDISPONÍVEL)\n");
-    printf("4 - Ir para venda (INDISPONÍVEL)\n");
-    printf("5 - SAIR\n");
-    printf("Escolha uma opcao: ");
-    scanf("%d", &opc);
-    setbuf(stdin, NULL);
-    printf("\n");
+void att_estoque(comp *c) {
+    if (cont_comp < 0) {
+        printf("Nenhum componente cadastrado.\n");
+        return;
+    }
 
-    return(opc);
+    int codigo, qtd;
+    printf("Digite o código do componente para atualizar o estoque: ");
+    scanf("%d", &codigo);
+
+    for (int i = 0; i <= cont_comp; i++) {
+        if ((c + i)->codigo == codigo) {
+            printf("Quantidade atual: %d\n", (c + i)->quantidade);
+            printf("Digite a quantidade para adicionar ao estoque: "); 
+            scanf("%d", &qtd);
+            (c + i)->quantidade += qtd; 
+            printf("Estoque atualizado. Nova quantidade: %d\n", (c + i)->quantidade);
+            return;
+        }
+    }
+    printf("Componente não encontrado.\n");
 }
 
-void cad_prod(prod *p) {
-//refazer cadastro//
+void remover_comp(comp *c) {
+    if (cont_comp < 0) {
+        printf("Nenhum componente para remover.\n");
+        return;
+    }
+
+    int codigo, i, j;
+    printf("Digite o código do componente a ser removido: ");
+    scanf("%d", &codigo);
+
+    for (i = 0; i <= cont_comp; i++) {
+        if ((c + i)->codigo == codigo) {
+            printf("Componente '%s' removido.\n", (c + i)->nome);
+            for (j = i; j < cont_comp; j++) {
+                *(c + j) = *(c + j + 1);
+            }
+            cont_comp--;
+            return;
+        }
+    }
+    printf("Componente não encontrado.\n");
 }
 
-void listar_prod(prod *p) {
-//refazer lista de produtos//
-    
+void buscar_comp(comp *c) {
+    if (cont_comp < 0) {
+        printf("Nenhum componente cadastrado.\n");
+        return;
+    }
+
+    int codigo;
+    printf("Digite o código do componente a buscar: ");
+    scanf("%d", &codigo);
+
+    for (int i = 0; i <= cont_comp; i++) {
+        if ((c + i)->codigo == codigo) {
+            printf("\n--- Componente Encontrado ---\n");
+            printf("Código: %d\n", (c + i)->codigo);
+            printf("Nome: %s\n", (c + i)->nome);
+            printf("Marca: %s\n", (c + i)->marca);
+            printf("Descrição: %s\n", (c + i)->descricao);
+            printf("Preço: R$ %.2f\n", (c + i)->preco);
+            printf("Quantidade disponível: %d\n", (c + i)->quantidade);
+            return;
+        }
+    }
+    printf("Componente não encontrado.\n");
 }
 
-void att_estoque(prod *p){
-    //aumentar estoque de produto ou remover produto.//
-}
+void vender(comp *c) {
+    if (cont_comp < 0) {
+        printf("Nenhum componente disponível para venda.\n");
+        return;
+    }
 
-void vender(prod *p){
-    //a parte de vendas, onde a pessoa escolherá os produtos, ver preco, descricao, validade e quantidade disponível do produto//
-    //depois veremos quantos e quais produtos foram vendidos e o total da compra.//
+    int codigo, quantidade_venda;
+    printf("Digite o código do componente que deseja vender: ");
+    scanf("%d", &codigo);
+
+    for (int i = 0; i <= cont_comp; i++) {
+        if ((c + i)->codigo == codigo) {
+            printf("Quantidade disponível: %d\n", (c + i)->quantidade);
+            printf("Digite a quantidade para venda: ");
+            scanf("%d", &quantidade_venda);
+
+            if (quantidade_venda > (c + i)->quantidade) {
+                printf("Quantidade insuficiente em estoque.\n");
+                return;
+            }
+
+            (c + i)->quantidade -= quantidade_venda;
+            printf("Venda realizada com sucesso!\n");
+            printf("Emitindo nota fiscal...\n");
+
+            
+            strcpy((c + i)->notaFiscal, "NF-0001"); 
+
+            printf("\n--- Nota Fiscal ---\n");
+            printf("%s\n", (c + i)->notaFiscal);
+            printf("Componente: %s\n", (c + i)->nome);
+            printf("Marca: %s\n", (c + i)->marca);
+            printf("Quantidade Vendida: %d\n", quantidade_venda);
+            printf("Valor Total: R$ %.2f\n", quantidade_venda * (c + i)->preco);
+            printf("--------------------\n");
+            return;
+        }
+    }
+    printf("Componente não encontrado.\n");
 }
